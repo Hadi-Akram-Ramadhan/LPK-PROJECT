67575a239a6cdbfda4ccecf140b2493ab0bd5737
Hadi-Akram-Ramadhan <kk.hadi.akram@gmail.com>
2026-04-10 18:09:26 +0700

    feat: initialize DatabaseSeeder with default admin user account

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,43 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Buat satu kelas dummy
-        $kelas = Kelas::create([
-            'nama' => 'Bahasa Korea EPS TOPIK'
-        ]);
-
-        // Akun Admin
+        // Akun Admin Utama
         User::create([
             'name' => 'Admin Utama',
             'email' => 'admin@lpk.com',
             'password' => Hash::make('password'),
             'role' => 'admin',
-        ]);
-
-        // Akun Guru
-        User::create([
-            'name' => 'Sensei Yamada',
-            'email' => 'guru@lpk.com',
-            'password' => Hash::make('password'),
-            'role' => 'guru',
-        ]);
-
-        // Akun Murid 1
-        User::create([
-            'name' => 'Siswa Budi',
-            'email' => 'murid1@lpk.com',
-            'password' => Hash::make('password'),
-            'role' => 'murid',
-            'kelas_id' => $kelas->id,
-        ]);
-
-        // Akun Murid 2
-        User::create([
-            'name' => 'Siswa Ani',
-            'email' => 'murid2@lpk.com',
-            'password' => Hash::make('password'),
-            'role' => 'murid',
-            'kelas_id' => $kelas->id,
         ]);
     }
 }
